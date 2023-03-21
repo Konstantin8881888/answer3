73bb829f675d4a8001e19e13f56206313a4f177a
Konstantin <kamagoroff@gmail.com>
2023-03-21 15:36:48 +0300

    Update

--- src/main/java/com/example/answer3/pingPong/PingPong.java
+++ src/main/java/com/example/answer3/pingPong/PingPong.java
@@ -1,2 +1,25 @@
-package com.example.answer3.pingPong;public class PingPong {
+package com.example.answer3.pingPong;
+
+public class PingPong {
+    private static final int MAX_COUNT = 30;
+
+    public static void main(String[] args) {
+        PingPongExecute pongExecute = new PingPongExecute();
+
+        Thread pingThread = new Thread(() -> {
+            for (int i = 0; i < MAX_COUNT; i++) {
+                pongExecute.playPing();
+            }
+        });
+
+        Thread pongThread = new Thread(() -> {
+            for (int i = 0; i < MAX_COUNT; i++) {
+                pongExecute.playPong();
+            }
+        });
+
+        pingThread.start();
+        pongThread.start();
+    }
 }
+
