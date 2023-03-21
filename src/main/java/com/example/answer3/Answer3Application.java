6e6cb24ae39872ee47f11529a2ec90327a630222
Konstantin <kamagoroff@gmail.com>
2023-03-21 13:51:15 +0300

    Init

---
+++ src/main/java/com/example/answer3/Answer3Application.java
@@ -0,0 +1,13 @@
+package com.example.answer3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Answer3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Answer3Application.class, args);
+	}
+
+}
